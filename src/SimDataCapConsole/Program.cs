bd7f1393d22e1adac4c71ef15a816f44c1ddb7c3
John Sedlak <john.sedlak@outlook.com>
2024-03-25 21:13:54 -0400

    Updates

---
+++ src/SimDataCapConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using SimDataCapConsole;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+var simCap = new SimCap();
+simCap.Start();
+
+bool isRunning = true;
+while (!Console.KeyAvailable && isRunning)
+{
+    switch(Console.ReadKey(true).Key)
+    {
+        case ConsoleKey.Q:
+            isRunning = false;
+            break;
+    }
+}
+
+Log.Logger.Information("Quitting...");
+
+simCap.Stop();
---
+++ src/SimDataCapConsole/SimCap.cs
@@ -0,0 +1,61 @@
+﻿using HerboldRacing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimDataCapConsole;
+
+internal class SimCap
+{
+    private readonly IRSDKSharper _irSdk;
+
+    private List<object> _data = new();
+
+    public SimCap()
+    {
+        _irSdk = new IRSDKSharper();
+        _irSdk.OnConnected += OnConnected;
+        _irSdk.OnDisconnected += OnDisconnected;
+        _irSdk.OnSessionInfo += OnSessionInfo;
+        _irSdk.OnTelemetryData += OnTelemetryData;
+    }
+
+    public void Start()
+    {
+        _irSdk.Start();
+    }
+
+    public void Stop()
+    {
+        _irSdk.Stop();
+    }
+
+    private void OnTelemetryData()
+    {
+        var lapDistPct = _irSdk.Data.GetInt("")
+            //.Data.GetFloat("CarIdxLapDistPct", 5);
+
+        _data.Add(new
+        {
+            track = _irSdk.Data.SessionInfo?.WeekendInfo.TrackName,
+
+        })
+    }
+
+    private void OnSessionInfo()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnDisconnected()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnConnected()
+    {
+        
+    }
+}
